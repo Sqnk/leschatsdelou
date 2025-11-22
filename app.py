ed965ea54cc98efe837e88710ad15ca0c18b068e
Sqnk <44092330+Sqnk@users.noreply.github.com>
2025-11-22 04:12:32 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 from datetime import datetime, date, timedelta
-from flask import Flask, render_template, request, redirect, url_for, send_from_directory, flash, jsonify, session
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory, flash, jsonify, session, send_file
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import inspect
 from sqlalchemy import text
